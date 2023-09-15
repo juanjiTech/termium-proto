2b915f7e00bf6c1aadefb21cf5a4ac04bb4e526e
杨成锴 <homeboyc@foxmail.com>
2023-09-16 01:10:14 +0800

    fix: fix ci error

---
+++ gen/proto/auth/v1/auth_service.pb.ts
@@ -0,0 +1,49 @@
+/* eslint-disable */
+// @ts-nocheck
+/*
+* This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
+*/
+
+import * as fm from "../../fetch.pb"
+export type RegisterRequest = {
+  email?: string
+  verifyCode?: string
+  password?: string
+}
+
+export type LoginResponse = {
+  accessToken?: string
+  refreshToken?: string
+}
+
+export type RegisterResponse = {
+  accessToken?: string
+  refreshToken?: string
+}
+
+export type LoginRequest = {
+  username?: string
+  password?: string
+  twoFactorAuth?: string
+}
+
+export type RefreshTokenRequest = {
+  refreshToken?: string
+}
+
+export type RefreshTokenResponse = {
+  accessToken?: string
+  refreshToken?: string
+}
+
+export class AuthService {
+  static Register(req: RegisterRequest, initReq?: fm.InitReq): Promise<RegisterResponse> {
+    return fm.fetchReq<RegisterRequest, RegisterResponse>(`/gapi/auth/v1/register`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+  static Login(req: LoginRequest, initReq?: fm.InitReq): Promise<LoginResponse> {
+    return fm.fetchReq<LoginRequest, LoginResponse>(`/gapi/auth/v1/login`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+  static RefreshToken(req: RefreshTokenRequest, initReq?: fm.InitReq): Promise<RefreshTokenResponse> {
+    return fm.fetchReq<RefreshTokenRequest, RefreshTokenResponse>(`/gapi/auth/v1/refreshToken`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+}
---
+++ gen/proto/user/v1/user_service.pb.ts
@@ -0,0 +1,41 @@
+/* eslint-disable */
+// @ts-nocheck
+/*
+* This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
+*/
+
+import * as fm from "../../fetch.pb"
+import * as UserV1User from "./user.pb"
+export type GetInfoRequest = {
+}
+
+export type GetInfoResponse = {
+  info?: UserV1User.User
+}
+
+export type SendEmailVerifyCodeRequest = {
+  email?: string
+}
+
+export type SendEmailVerifyCodeResponse = {
+}
+
+export type CheckEmailVerifyCodeRequest = {
+  email?: string
+  code?: string
+}
+
+export type CheckEmailVerifyCodeResponse = {
+}
+
+export class UserService {
+  static GetInfo(req: GetInfoRequest, initReq?: fm.InitReq): Promise<GetInfoResponse> {
+    return fm.fetchReq<GetInfoRequest, GetInfoResponse>(`/gapi/user/info?${fm.renderURLSearchParams(req, [])}`, {...initReq, method: "GET"})
+  }
+  static SendEmailVerifyCode(req: SendEmailVerifyCodeRequest, initReq?: fm.InitReq): Promise<SendEmailVerifyCodeResponse> {
+    return fm.fetchReq<SendEmailVerifyCodeRequest, SendEmailVerifyCodeResponse>(`/gapi/user/v1/verify/email/send`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+  static CheckEmailVerifyCode(req: CheckEmailVerifyCodeRequest, initReq?: fm.InitReq): Promise<CheckEmailVerifyCodeResponse> {
+    return fm.fetchReq<CheckEmailVerifyCodeRequest, CheckEmailVerifyCodeResponse>(`/gapi/user/v1/verify/email/check`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+}
