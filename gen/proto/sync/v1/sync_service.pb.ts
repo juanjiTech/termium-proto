8ac33458083e83c5b571e07a1acafec2faa210d9
asjdf <asjdf@users.noreply.github.com>
2026-07-01 04:22:26 +0000

    update: automatic generate new proto

--- gen/proto/sync/v1/sync_service.pb.ts
+++ gen/proto/sync/v1/sync_service.pb.ts
@@ -5,6 +5,7 @@
 */
 
 import * as fm from "../../fetch.pb"
+import * as GoogleProtobufTimestamp from "../../google/protobuf/timestamp.pb"
 import * as TeamV1Team from "../../team/v1/team.pb"
 import * as UserV1Key_wallet from "../../user/v1/key_wallet.pb"
 import * as SyncV1Host from "./host.pb"
@@ -21,12 +22,12 @@ type OneOf<T> =
         : never)
     : never);
 export type SyncConfigRequest = {
-  after?: Date
+  after?: GoogleProtobufTimestamp.Timestamp
   teamId?: string
 }
 
 export type SyncConfigResponse = {
-  serverTime?: Date
+  serverTime?: GoogleProtobufTimestamp.Timestamp
   hostSet?: SyncV1Host.Host[]
   knownHostSet?: SyncV1Known_hosts.KnownHost[]
   sshKeySet?: SyncV1Keychain.SshKey[]
@@ -44,27 +45,27 @@ export type UpdateConfigRequest = BaseUpdateConfigRequest
 
 
 type BaseUpdateConfigResponse = {
-  serverTime?: Date
+  serverTime?: GoogleProtobufTimestamp.Timestamp
 }
 
 export type UpdateConfigResponse = BaseUpdateConfigResponse
   & OneOf<{ host: SyncV1Host.Host; knownHost: SyncV1Known_hosts.KnownHost; sshKey: SyncV1Keychain.SshKey; identity: SyncV1Keychain.Identity; portForward: SyncV1Port_forward.PortForward }>
 
 export type SyncTeamRequest = {
-  after?: Date
+  after?: GoogleProtobufTimestamp.Timestamp
 }
 
 export type SyncTeamResponse = {
-  serverTime?: Date
+  serverTime?: GoogleProtobufTimestamp.Timestamp
   teams?: TeamV1Team.Team[]
 }
 
 export type SyncUserKeyWalletRequest = {
-  after?: Date
+  after?: GoogleProtobufTimestamp.Timestamp
 }
 
 export type SyncUserKeyWalletResponse = {
-  serverTime?: Date
+  serverTime?: GoogleProtobufTimestamp.Timestamp
   publicKey?: string
   encryptedPrivateKey?: string
   userKeyWalletSet?: UserV1Key_wallet.UserKeyWallet[]
@@ -80,7 +81,7 @@ export type UpdateUserKeyWalletResponse = {
   publicKey?: string
   encryptedPrivateKey?: string
   userKeyWalletSet?: UserV1Key_wallet.UserKeyWallet[]
-  serverTime?: Date
+  serverTime?: GoogleProtobufTimestamp.Timestamp
 }
 
 export class SyncService {
